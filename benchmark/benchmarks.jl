79af57c647369c7fb0aec9753868799d908b67d6
Alexander Samoilov <alexander.samoilov@gmail.com>
2016-08-20 22:22:49 +0300

    fix `update?()` and add `update?_eachindex()`.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -8,11 +8,15 @@ y = OffsetArray(Float64, -dim + 1 : dim)
 
 fillx() = for i = 1:2*dim x[i] = i end
 filly() = for i = -dim+1:dim y[i] = i end
-updatex() = for i = 1:2*dim x[i] = i end
-updatey() = for i = -dim+1:dim y[i] = i end
+updatex() = for i = 1:2*dim x[i] = x[i] + i end
+updatey() = for i = -dim+1:dim y[i] = y[i] + i end
+updatex_eachindex() = for i in eachindex(x) x[i] + i end
+updatey_eachindex() = for i in eachindex(y) y[i] = y[i] + i end
 
-@benchmark fillx() 
+@benchmark fillx()
 @benchmark filly()
-@benchmark updatex() 
+@benchmark updatex()
 @benchmark updatey()
+@benchmark updatex_eachindex()
+@benchmark updatey_eachindex()
 
