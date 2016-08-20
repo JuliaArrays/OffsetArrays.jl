22c4f606f3830484b98e1ac86be7fa249c4f191c
Alexander Samoilov <alexander.samoilov@gmail.com>
2016-08-20 23:34:13 +0300

    fix `updatex_eachindex`.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -10,7 +10,7 @@ fillx() = for i = 1:2*dim x[i] = i end
 filly() = for i = -dim+1:dim y[i] = i end
 updatex() = for i = 1:2*dim x[i] = x[i] + i end
 updatey() = for i = -dim+1:dim y[i] = y[i] + i end
-updatex_eachindex() = for i in eachindex(x) x[i] + i end
+updatex_eachindex() = for i in eachindex(x) x[i] = x[i] + i end
 updatey_eachindex() = for i in eachindex(y) y[i] = y[i] + i end
 
 @benchmark fillx()
