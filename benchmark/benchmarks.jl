16653b0105a78a6ca389cab85ef73887fc282caa
Tim Holy <tim.holy@gmail.com>
2016-08-20 16:47:45 -0500

    Don't use global variables in the benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,17 +6,13 @@ const dim = 1000
 x = Array(Float64, 2*dim)
 y = OffsetArray(Float64, -dim + 1 : dim)
 
-fillx() = for i = 1:2*dim x[i] = i end
-filly() = for i = -dim+1:dim y[i] = i end
-updatex() = for i = 1:2*dim x[i] = x[i] + i end
-updatey() = for i = -dim+1:dim y[i] = y[i] + i end
-updatex_eachindex() = for i in eachindex(x) x[i] = x[i] + i end
-updatey_eachindex() = for i in eachindex(y) y[i] = y[i] + i end
+fillx(x) = for i in indices(x,1); x[i] = i; end
+updatex(x) = for i in indices(x,1); x[i] = x[i] + i; end
+updatex_eachindex(x) = for i in eachindex(x); x[i] = x[i] + i; end
 
-@benchmark fillx()
-@benchmark filly()
-@benchmark updatex()
-@benchmark updatey()
-@benchmark updatex_eachindex()
-@benchmark updatey_eachindex()
-
+@show @benchmark fillx(x)
+@show @benchmark fillx(y)
+@show @benchmark updatex(x)
+@show @benchmark updatex(y)
+@show @benchmark updatex_eachindex(x)
+@show @benchmark updatex_eachindex(y)
