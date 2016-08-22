08f209fe70c488bd710986ed98475b8edc3d965c
Tim Holy <tim.holy@gmail.com>
2016-08-22 16:18:53 -0500

    Benchmarks: traverse arrays in cache order

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -9,9 +9,9 @@ x2d = Array(Float64, 2*dim, 2*dim)
 y2d = OffsetArray(Float64, -dim + 1 : dim, -dim + 1 : dim)
 
 fill(x) = for i in indices(x,1); x[i] = i; end
-fill2d(x) = for i in indices(x,1); for j in indices(x,2); x[i,j] = i + j; end; end
+fill2d(x) = for j in indices(x,2); for i in indices(x,1); x[i,j] = i + j; end; end
 update(x) = for i in indices(x,1); x[i] = x[i] + i; end
-update2d(x) = for i in indices(x,1); for j in indices(x,2); x[i,j] = x[i,j] + i + j; end; end
+update2d(x) = for j in indices(x,2); for i in indices(x,1); x[i,j] = x[i,j] + i + j; end; end
 update_eachindex(x) = for i in eachindex(x); x[i] = x[i] + i; end
 
 @show @benchmark fill(x)
