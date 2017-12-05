5d57c37cdf4cf7cdd31daeaab22cc771bf010557
Tim Holy <tim.holy@gmail.com>
2017-12-04 20:16:21 -0600

    Fix display of 0-d OffsetArrays on 0.7 and higher
    Fixes #35

--- test/runtests.jl
+++ test/runtests.jl
@@ -358,6 +358,8 @@ if VERSION >= v"0.7.0-DEV.1790"
     a = OffsetArray([1 2; 3 4], -1:0, 5:6)
     @test summary(a) == "OffsetArray(::Array{$(Int),2}, -1:0, 5:6) with eltype $(Int) with indices -1:0×5:6"
     @test summary(view(a, :, 5)) == "view(OffsetArray(::Array{Int64,2}, -1:0, 5:6), :, 5) with eltype Int64 with indices -1:0"
+    a = OffsetArray(reshape([1]))
+    @test summary(a) == "0-dimensional OffsetArray(::Array{Int64,0}) with eltype Int64"
 end
 
 @testset "OffsetVector constructors" begin
