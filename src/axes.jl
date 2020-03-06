f683dc46508f86e5e197b913370e7c8240785b53
Johnny Chen <johnnychen94@hotmail.com>
2020-03-06 20:47:45 +0800

    route offset1 computing to index-preserving case

--- src/axes.jl
+++ src/axes.jl
@@ -128,12 +128,9 @@ offset_coerce(::Type{I}, r::AbstractUnitRange) where I<:AbstractUnitRange{T} whe
 @inline Base.axes1(r::IdOffsetRange) = IdOffsetRange(Base.axes1(r.parent), r.offset)
 @inline Base.unsafe_indices(r::IdOffsetRange) = (r,)
 @inline Base.length(r::IdOffsetRange) = length(r.parent)
-@inline function Base.compute_linindex(f, s, IP::Tuple, I::Tuple{IdOffsetRange, Vararg{Any}})
-    # issue 100
-    # IdOffsetRange doesn't contributes to offsets before the construction of SubArray
-    Δi =  1 - first(IP[1])
-    Base.compute_linindex(f + Δi*s, s*Base.unsafe_length(IP[1]), Base.tail(IP), Base.tail(I))
-end
+# issue 100: IdOffsetRange as another index-preserving case shouldn't comtribute offsets
+@inline Base.compute_offset1(parent, stride1::Integer, dims::Tuple{Int}, inds::Tuple{IdOffsetRange}, I::Tuple) =
+    Base.compute_linindex(parent, I) - stride1*first(axes(parent, dims[1]))
 
 @inline function Base.iterate(r::IdOffsetRange)
     ret = iterate(r.parent)
