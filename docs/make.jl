21d63d4c94310da3d009e4ba73042e17dde2bb60
Johnny Chen <johnnychen94@hotmail.com>
2020-09-14 19:37:20 +0800

    enable push_preview

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
-using Documenter
+using Documenter, JSON
 using OffsetArrays
 
 makedocs(
@@ -9,6 +9,17 @@ makedocs(
     doctestfilters = [r"at \./.*", r"at /home.*", r"top-level scope.*", r"\[\d*\]\s*$"],   # for backtraces
 )
 
+# a workdaround to github action that only push preview when PR has "push_preview" labels
+# issue: https://github.com/JuliaDocs/Documenter.jl/issues/1225
+function should_push_preview(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing))
+    event_path === nothing && return false
+    event = JSON.parsefile(event_path)
+    haskey(event, "pull_request") || return false
+    labels = [x["name"] for x in event["pull_request"]["labels"]]
+    return "push_preview" in labels
+ end
+
 deploydocs(
-    repo = "github.com:JuliaArrays/OffsetArrays.jl.git"
+    repo = "github.com:JuliaArrays/OffsetArrays.jl.git",
+    push_preview = should_push_preview()
 )
