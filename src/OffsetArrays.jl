6b3405bbc02e0b640bba4a9df51ed7ed3ab17969
Chris Elrod <elrodc@gmail.com>
2020-11-30 04:38:55 -0500

    pointer -> Base.unsafe_convert.

--- src/OffsetArrays.jl
+++ src/OffsetArrays.jl
@@ -294,7 +294,7 @@ end
     A
 end
 
-@inline Base.unsafe_convert(::Type{Ptr{T}}, A::OffsetArray{T}) where {T} = pointer(parent(A))
+@inline Base.unsafe_convert(::Type{Ptr{T}}, A::OffsetArray{T}) where {T} = Base.unsafe_convert(Ptr{T}, parent(A))
 
 # For fast broadcasting: ref https://discourse.julialang.org/t/why-is-there-a-performance-hit-on-broadcasting-with-offsetarrays/32194
 Base.dataids(A::OffsetArray) = Base.dataids(parent(A))
