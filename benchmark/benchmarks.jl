0749d26b8e8d3c7cf9846b45c898b75f805723a8
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2020-12-26 08:00:43 +0400

    Update the docstring for OffsetArray (#176)
    Rename fill to fill1d in benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -8,7 +8,7 @@ y = OffsetArray{Float64}(undef, -dim + 1 : dim)
 x2d = Array{Float64}(undef, 2*dim, 2*dim)
 y2d = OffsetArray{Float64}(undef, -dim + 1 : dim, -dim + 1 : dim)
 
-fill(x) = for i in axes(x,1); x[i] = i; end
+fill1d(x) = for i in axes(x,1); x[i] = i; end
 fill2d(x) = for j in axes(x,2); for i in axes(x,1); x[i,j] = i + j; end; end
 update(x) = for i in axes(x,1); x[i] = x[i] + i; end
 update2d(x) = for j in axes(x,2); for i in axes(x,1); x[i,j] = x[i,j] + i + j; end; end
@@ -20,8 +20,8 @@ unsafe_update(x) = @inbounds(for i in axes(x,1); x[i] = x[i] + i; end)
 unsafe_update2d(x) = @inbounds(for j in axes(x,2); for i in axes(x,1); x[i,j] = x[i,j] + i + j; end; end)
 unsafe_update_eachindex(x) = @inbounds(for i in eachindex(x); x[i] = x[i] + i; end)
 
-@show @benchmark fill(x)
-@show @benchmark fill(y)
+@show @benchmark fill1d(x)
+@show @benchmark fill1d(y)
 @show @benchmark fill2d(x2d)
 @show @benchmark fill2d(y2d)
 @show @benchmark update(x)
