5dd071750ec8004f7e5508cdcb7c275f3ee76f87
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2021-02-11 11:55:55 +0400

    fix the displayed indices in show(::OffsetRange) (#200)
    * fix indices in show(::OffsetRange)
    
    * Do not show type of the parent range, display only the values and indices

--- src/OffsetArrays.jl
+++ src/OffsetArrays.jl
@@ -367,9 +367,8 @@ indexing is faster with ranges =#
 @propagate_inbounds Base.getindex(r::UnitRange{<:Integer}, s::IIUR) = IdentityUnitRange(r[no_offset_view(s)])
 
 function Base.show(io::IO, r::OffsetRange)
-    show(io, r.parent)
-    o = r.offsets[1]
-    print(io, " with indices ", o+1:o+length(r))
+    show(io, UnitRange(r.parent))
+    print(io, " with indices ", UnitRange(axes(r, 1)))
 end
 Base.show(io::IO, ::MIME"text/plain", r::OffsetRange) = show(io, r)
 
