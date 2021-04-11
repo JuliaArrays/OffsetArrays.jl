bf24c31fcd076b2fe281b0a66b1fab29cdbc7e58
Johnny Chen <johnnychen94@hotmail.com>
2021-04-11 20:34:31 +0800

    [compat] copy helpers from Base
    For better compatibility to old and future Julia versions

--- src/utils.jl
+++ src/utils.jl
@@ -17,21 +17,27 @@ function _offset_reshape_uncolon(A::AbstractArray, I::Tuple{OffsetAxis,Vararg{Of
     @noinline throw2(A, I) = throw(DimensionMismatch(string("array size $(length(A)) ",
         "must be divisible by the product of the new dimensions $I")))
 
-    pre = Base._before_colon(I...)
-    post = Base._after_colon(I...)
-    Base._any_colon(post...) && throw1(dims)
+    pre = _before_colon(I...)
+    post = _after_colon(I...)
+    _any_colon(post...) && throw1(dims)
     nprev = isempty(pre) ? 1 : mapreduce(_length, *, pre)
     npost = isempty(post) ? 1 : mapreduce(_length, *, post)
     sz, remainder = divrem(length(A), nprev * npost)
     remainder == 0 || throw2(A, dims)
 
+    # preserve the offset information, the default offset beyond `ndims(A)` is 0
     n = length(pre)
     r = Base.OneTo(Int(sz))
-    # preserve the offset information
     Δ = n < ndims(A) ? _offset(axes(A, n+1), r) : 0
     (pre..., IdOffsetRange(r, -Δ), post...)
 end
-
+@inline _any_colon() = false
+@inline _any_colon(r::Colon, tail...) = true
+@inline _any_colon(r::Any, tail...) = _any_colon(tail...)
+@inline _before_colon(r::Any, tail...) = (r, _before_colon(tail...)...)
+@inline _before_colon(r::Colon, tail...) = ()
+@inline _after_colon(r::Any, tail...) =  _after_colon(tail...)
+@inline _after_colon(r::Colon, tail...) = tail
 @inline _length(r::AbstractUnitRange) = length(r)
 @inline _length(n::Int) = n
 
