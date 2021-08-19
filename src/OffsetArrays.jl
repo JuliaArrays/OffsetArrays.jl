ca63d806957663c65113a0a37bbc9a0a297200a8
Johnny Chen <johnnychen94@hotmail.com>
2021-08-19 11:05:41 +0800

    More clear explaination on `centered`

--- src/OffsetArrays.jl
+++ src/OffsetArrays.jl
@@ -705,8 +705,8 @@ end
 """
     centered(A, cp=center(A)) -> Ao
 
-Shift the center coordinate of array `A` to `(0, 0, ...)`. If `size(A, k)`
-is even, a rounding procedure will be applied with mode `r`.
+Shift the center coordinate/point `cp` of array `A` to `(0, 0, ..., 0)`. Internally, this is
+equivalent to `OffsetArray(A, .-cp)`.
 
 !!! compat "OffsetArrays 1.9"
     This method requires at least OffsetArrays 1.9.
@@ -731,6 +731,29 @@ julia> Aoo = OffsetArrays.centered(Ao); # axes (-1:1, -1:1)
 
 julia> Aoo[0, 0]
 5
+```
+
+Users are allowed to pass `cp` to change how "center point" is interpreted, but the meaning of the
+output array should be reinterpreted as well. For instance, if `cp = map(last, axes(A))` then this
+function no longer shifts the center point but instead the bottom-right point to `(0, 0, ..., 0)`.
+A commonly usage of `cp` is to change the rounding behavior when the array is of even size at some
+dimension:
+
+```jldoctest; setup=:(using OffsetArrays)
+julia> A = reshape(collect(1:4), 2, 2) # Ideally the center should be (1.5, 1.5) but OffsetArrays only support integer offsets
+2×2 $(Matrix{Int}):
+ 1  3
+ 2  4
+
+julia> OffsetArrays.centered(A, OffsetArrays.center(A, RoundUp)) # set (2, 2) as the center point
+2×2 OffsetArray(::$(Matrix{Int}), -1:0, -1:0) with eltype $(Int) with indices -1:0×-1:0:
+ 1  3
+ 2  4
+
+julia> OffsetArrays.centered(A, OffsetArrays.center(A, RoundDown)) # set (1, 1) as the center point
+2×2 OffsetArray(::$(Matrix{Int}), 0:1, 0:1) with eltype $(Int) with indices 0:1×0:1:
+ 1  3
+ 2  4
 ```
 
 See also [`center`](@ref OffsetArrays.center).
