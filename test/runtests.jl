6f5abaa7e19e00fa5f1e38b602ea2c96ee7160e4
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-05-20 11:39:29 +0400

    fix searchsorted insertion indices

--- test/runtests.jl
+++ test/runtests.jl
@@ -2332,16 +2332,16 @@ end
     @test searchsortedlast(soa, 1) == typemin(Int)
 
     soa = OffsetArray([2,2,3], typemax(Int)-4)
-    @test searchsorted(soa, 1) == firstindex(soa) .+ (1:0)
+    @test searchsorted(soa, 1) === firstindex(soa) .+ (0:-1)
     @test searchsorted(soa, 2) == firstindex(soa) .+ (0:1) == typemax(Int) .+ (-3:-2)
     @test searchsorted(soa, 3) == lastindex(soa) .+ (0:0) == typemax(Int) .+ (-1:-1)
-    @test searchsorted(soa, 4) == lastindex(soa) .+ (1:0)
+    @test searchsorted(soa, 4) === lastindex(soa) .+ (1:0)
 
     soa = OffsetArray([2,2,3], typemax(Int)-3)
-    @test searchsorted(soa, 1) == firstindex(soa) .+ (1:0)
+    @test searchsorted(soa, 1) === firstindex(soa) .+ (0:-1)
     @test_broken searchsorted(soa, 2) == firstindex(soa) .+ (0:1) == typemax(Int) .+ (-2:-1)
     @test_broken searchsorted(soa, 3) == firstindex(soa) .+ (0:0) == typemax(Int) .+ (0:0)
-    @test searchsorted(soa, 4) == lastindex(soa) .+ (1:0)
+    @test searchsorted(soa, 4) === lastindex(soa) .+ (1:0)
 end
 
 @testset "Adapt" begin
