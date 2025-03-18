be8672cca133cdf5afa51485672c0707a3b090e8
William Moses <gh@wsmoses.com>
2025-03-18 22:25:37 +0100

    Implement Adapt.parent_type

--- ext/OffsetArraysAdaptExt.jl
+++ ext/OffsetArraysAdaptExt.jl
@@ -7,5 +7,5 @@ using OffsetArrays, Adapt
 ##
 import Adapt
 Adapt.adapt_structure(to, O::OffsetArray) = OffsetArrays.parent_call(x -> Adapt.adapt(to, x), O)
-
+Adapt.parent_type(::Type{OffsetArray{<:Any, <:Any, AA}}) where AA = AA
 end
