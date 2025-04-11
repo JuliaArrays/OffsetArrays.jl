241b1b2ed18ef617755efb26066898a2b3e966bc
Valentin Churavy <v.churavy@gmail.com>
2025-04-11 15:30:22 +0200

    implement unwrap_type from Adapt

--- ext/OffsetArraysAdaptExt.jl
+++ ext/OffsetArraysAdaptExt.jl
@@ -11,6 +11,7 @@ Adapt.adapt_structure(to, O::OffsetArray) = OffsetArrays.parent_call(x -> Adapt.
 @static if isdefined(Adapt, :parent_type)
     # To support Adapt 3.0 which doesn't have parent_type defined
     Adapt.parent_type(::Type{OffsetArray{T,N,AA}}) where {T,N,AA} = AA
+    Adapt.unwrap_type(W::Type{<:OffsetArray}) = unwrap_type(parent_type(W))
 end
 
 end
